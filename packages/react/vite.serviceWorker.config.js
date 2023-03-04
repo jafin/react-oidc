ba1e71fd6617d3b4af0d7491a519cd95c96c28ee
Jason Finch <jason.finch@gmail.com>
2023-03-04 11:31:48 +1000

    integrate serviceworker build into react folder, remove requirement to npm i in subfolder.  Note: to do this, need to add type:module to package.json which then has onflow effects for eslint files (cjs)

--- packages/react/vite.serviceWorker.config.js
+++ packages/react/vite.serviceWorker.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     sourcemap: true,
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, './OidcServiceWorker.ts'),
+      entry: resolve(__dirname, './service_worker/OidcServiceWorker.ts'),
       name: 'OidcServiceWorker',
       formats: ['es'],
       // the proper extensions will be added
